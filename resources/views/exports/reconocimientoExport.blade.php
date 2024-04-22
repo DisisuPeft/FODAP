9e7d14b8073c5042d85b9d53116dc07eb150b585
DisiusPeft <defp240899@gmail.com>
2024-04-21 21:04:43 -0600

    01

--- resources/views/exports/reconocimientoExport.blade.php
+++ resources/views/exports/reconocimientoExport.blade.php
@@ -7,12 +7,6 @@
         <th>Periodo</th>
         <th>Fecha de imparticion</th>
         <th>Lugar y fecha de registro</th>
-        <th>Nombre Facilitador 1</th>
-        <th>Género 1</th>
-        <th>Nombre facilitador 2</th>
-        <th>Género 2</th>
-        <th>Nombre facilitador 3</th>
-        <th>Género 3</th>
         <th>Nivel educativo</th>
         <th>Forma de imparticion del curso</th>
         <th>Duración del Curso</th>
@@ -40,28 +34,6 @@
             <td>{{ $curso->periodo }}</td>
             <td>{{ $curso->fecha_imparticion }}</td>
             <td>{{ $curso->lugar_registro }}</td>
-            <td>{{ $curso->facilitador_1 }}</td>
-            <td>{{ $curso->genero_1 }}</td>
-            @if (isset($curso->facilitador_2))
-                <td>{{ $curso->facilitador_2 }}</td>
-            @else
-                <td></td>
-            @endif
-            @if (isset($curso->genero_2))
-                <td>{{ $curso->genero_2 }}</td>
-            @else
-                <td></td>
-            @endif
-            @if (isset($curso->facilitador_3))
-                <td>{{ $curso->facilitador_3 }}</td>
-            @else
-                <td></td>
-            @endif
-            @if (isset($curso->genero_3))
-                <td>{{ $curso->genero_3 }}</td>
-            @else
-                <td></td>
-            @endif
             <td>{{ $curso->nivel_educativo }}</td>
             <td>{{ $curso->modalidad }}</td>
             <td>{{ $curso->duracion }}</td>
