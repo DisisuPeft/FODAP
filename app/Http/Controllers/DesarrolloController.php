9f7ac2ee9066ac8950d02d85f0139fcec9f0e7ec
DisiusPeft <defp240899@gmail.com>
2024-04-28 16:37:04 -0600

    new checked

--- app/Http/Controllers/DesarrolloController.php
+++ app/Http/Controllers/DesarrolloController.php
@@ -387,7 +387,7 @@ class DesarrolloController extends Controller
         $totales = DB::table('docente')
             ->leftjoin('inscripcion', 'inscripcion.docente_id', '=', 'docente.id')
             ->whereColumn('inscripcion.docente_id', '=', 'docente.id')
-            ->select('docente.id')
+            ->select('docente.id', 'docente.nombre', 'docente.apellidoPat', 'docente.apellidoMat')
             ->distinct()
             ->get();
         // ->count();
